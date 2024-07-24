5dc003205b9b8c49c68294dea7463db337ca57e6
KaroKru <92943379+KaroKru@users.noreply.github.com>
2024-07-24 22:59:05 +0200

    added ci

--- source/BlurImage.py
+++ source/BlurImage.py
@@ -1,15 +1,15 @@
-from PIL import ImageFilter
-from ImageInformation import ImageHandler
+from PIL import Image, ImageFilter
 
-class EnhTechniques():
-    
+
+class EnhTechniques:
+
     def __init__(self, reader):
         self.reader = reader
-    
+
     def gaussianBlur(self):
         radius = int(input("Enter the value of the blur radius "))
         if radius < 0:
-            raise ValueError(f"An invalid value has been selected")
+            raise ValueError("An invalid value has been selected")
 
         gaussianMask = ImageFilter.GaussianBlur(radius)
         gaussianImage = self.reader.image.copy()
@@ -20,37 +20,47 @@ class EnhTechniques():
     def histogram(self):
         histogramImage = self.reader.image.copy()
         hist = histogramImage.histogram()
-        distribution = [sum(hist[:i+1]) for i in range(len(hist))]
-        distributionMap = [int(((distribution[i] - distribution[0]) / (histogramImage.width * histogramImage.height - distribution[0])) * 255) for i in range(len(distribution))]
+        distribution = [sum(hist[:i + 1]) for i in range(len(hist))]
+        distributionMap = [
+            int(((distribution[i] - distribution[0]) /
+                (histogramImage.width *
+                histogramImage.height - distribution[0])) * 255)
+            for i in range(len(distribution))
+        ]
         newImage = Image.new(histogramImage.mode, histogramImage.size)
         pixels = histogramImage.load()
         newPixels = newImage.load()
         for i in range(histogramImage.width):
             for j in range(histogramImage.height):
-                newPixels[i, j] = (distributionMap[pixels[i, j][0]], distributionMap[pixels[i, j][1]], distributionMap[pixels[i, j][2]])
+                newPixels[i, j] = (
+                    distributionMap[pixels[i, j][0]],
+                    distributionMap[pixels[i, j][1]],
+                    distributionMap[pixels[i, j][2]]
+                )
 
         self.reader.saveFile(newImage, "histogram")
 
     def averageHistogram(self):
         averageImage = self.reader.image.copy()
         smoothSize = (3, 3)
-        averageSmooth = [1/9] * 9
-        smoothed = averageImage.filter(ImageFilter.Kernel(smoothSize, averageSmooth))
+        averageSmooth = [1 / 9] * 9
+        smoothed = averageImage.filter(
+            ImageFilter.Kernel(smoothSize, averageSmooth))
 
         self.reader.saveFile(smoothed, "averageHistogram")
-    
+
     def checkOptions(self):
         techniques = [
-                "histogram", 
-                "gaussian blur", 
-                "average histogram"
+            "histogram",
+            "gaussian blur",
+            "average histogram"
         ]
 
-        print(f"Possible image enhancement techniques: ")
-            
-        for id, techniques in enumerate(techniques, start = 1):
-            print(f"{id} -> {techniques}")
-            
+        print("Possible image enhancement techniques:")
+
+        for id, technique in enumerate(techniques, start=1):
+            print(f"{id} -> {technique}")
+
         choose = int(input("What do you want to do? Enter an id value:"))
 
         match choose:
@@ -61,4 +71,4 @@ class EnhTechniques():
             case 3:
                 self.averageHistogram()
             case _:
-                print(f"Selected value out of the scope")
+                print("Selected value out of the scope")
--- source/main.py
+++ source/main.py
@@ -5,9 +5,10 @@ from BinaryModification import MorphoTransformation
 from BlurImage import EnhTechniques
 from CompresImage import CompressImage
 
+
 def samplePhotoInformation():
     print("Please add your own photo or select one of sample photos\n")
-    directory = os.path.join('..', 'photo')
+    directory = os.path.join("..", "photo")
 
     try:
         files = os.listdir(directory)
@@ -15,7 +16,7 @@ def samplePhotoInformation():
         for file in files:
             print(file)
     except FileNotFoundError:
-        print(f"The directory {directory} does not exist.")
+        print("The directory does not exist.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -25,39 +26,34 @@ def chooseOption(fileToChange):
         "Color space transformation",
         "Morphological transformations",
         "Image enhancement techniques",
-        "Image compression"
+        "Image compression",
     ]
 
-    print(f"Select one option from many possibles:")
-    for idx, option in enumerate(options, start = 1):
+    print("Select one option from many possibles:")
+    for idx, option in enumerate(options, start=1):
         print(f"{idx} -> {option}")
 
     answer = int(input("What do you want to do? Enter an id value: "))
-                
+
     match answer:
         case 1:
-            print(f"Selected colour space transformation")
-            
+            print("Selected colour space transformation")
             colour = ColourTransformation(fileToChange)
             colour.colourOptions()
         case 2:
-            print(f"Selected morphological transformations")
-            
+            print("Selected morphological transformations")
             binary = MorphoTransformation(fileToChange)
             binary.transformationOptions()
-        
-        case 3: 
-            print(f"Selected image enhancement techniques")
-            
+        case 3:
+            print("Selected image enhancement techniques")
             blur = EnhTechniques(fileToChange)
             blur.checkOptions()
-        case 4: 
-            print(f"Selected image compression")
-            
+        case 4:
+            print("Selected image compression")
             compression = CompressImage(fileToChange)
             compression.compress()
         case _:
-            print(f"Invalid value")
+            print("Invalid value")
 
 
 def openFile():
@@ -68,26 +64,27 @@ def openFile():
     while repeat == "yes":
         try:
             photoName = input("Enter the image name with the file extension: ")
-            photoPath = os.path.join('..', 'photo', photoName)
+            photoPath = os.path.join("..", "photo", photoName)
 
             if photoPath is None:
-                raise ValueError(f"Problem with file.")
+                raise ValueError("Problem with file.")
 
-            if photoPath.lower().endswith('.jpg'):
+            if photoPath.lower().endswith(".jpg"):
                 readPhoto = ImageHandler(photoPath)
                 readPhoto.openImage()
                 chooseOption(readPhoto)
             else:
-                raise ValueError(f"Choose other file type")
+                raise ValueError("Choose other file type")
 
         except Exception as e:
             print(f"Error: {e}")
             raise
 
-        userAnswear = input("Do you want to repat?")
-        repeat = userAnswear.lower()
-    
+        userAnswer = input("Do you want to repeat?")
+        repeat = userAnswer.lower()
+
     print("Closed")
+
 
 if __name__ == "__main__":
     openFile()
